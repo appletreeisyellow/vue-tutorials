830446ff9681076e015fac08862e9cce72fde2a9
Vivo Ye <yccvivo@gmail.com>
2020-06-30 22:00:50 -0700

    add code editor files

---
+++ real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Event Card</h1>
+  <div>
+    <ul>
+      <li>Yes</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style></style>
