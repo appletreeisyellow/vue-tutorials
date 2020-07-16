54ac98b72297d14b22e33019b4f4c75414c6526e
Vivo Ye <yccvivo@gmail.com>
2020-07-16 12:12:46 -0700

    add dynamic routes

--- vue-travel-app/src/views/DestinationDetails.vue
+++ vue-travel-app/src/views/DestinationDetails.vue
@@ -1,9 +1,48 @@
 <template>
-  <p>The destination is {{ this.$route.params.id }}</p>
+  <section class="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img :src="require(`@/assets/${destination.image}`)" :alt="destination.name" />
+      <p>{{ destination.description }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import store from "@/store.js";
+
+export default {
+  data() {
+    return {
+      destinationID: this.$route.params.id
+    };
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        destination => destination.id === this.destinationID
+      );
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
