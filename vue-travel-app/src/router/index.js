a75ed9863bb97b72e53e3f9a244acf02abceebd3
Vivo Ye <yccvivo@gmail.com>
2020-07-29 17:52:01 -0700

    add to control the scroll behavior of the vue router

--- vue-travel-app/src/router/index.js
+++ vue-travel-app/src/router/index.js
@@ -56,6 +56,23 @@ const routes = [
 const router = new VueRouter({
   mode: "history", // make URL look normal without "#" sign
   linkExactActiveClass: "vue-school-sctive-class",
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      const position = {};
+      if (to.hash) {
+        position.selector = to.hash;
+        if (to.hash === "#experience") {
+          position.offset = { y: 140 };
+        }
+        if (document.querySelector(to.hash)) {
+          return position;
+        }
+        return false;
+      }
+    }
+  },
   routes,
 });
 
